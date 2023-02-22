e3b3d3a69c5cd51f0ed86e68f84205d7c8001125
seoyeon <dltjdus0725@naver.com>
2023-02-22 23:57:16 +0900

    00

--- Portfolio_Feb/src/main/java/com/port/controller/HomeController.java
+++ Portfolio_Feb/src/main/java/com/port/controller/HomeController.java
@@ -41,7 +41,7 @@ public class HomeController {
 		int answer=0;
 		for(int i=0;i<arr.length;i++) {
 			for(int j=0;j<arr[i].length;j++) {
-				answer+=arr[i][j];
+				answer+=arr[i][j]; 
 			}
 		}
 		System.out.println(answer);
